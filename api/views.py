7cad1c91b2c4a49980957e814da446e6e29da4e8
marufiqbal5g <kmiqbal1989@gmail.com>
2026-04-13 15:29:47 +0600

    task-list

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+from .models import Task
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_views = {
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+    return Response(api_views)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
